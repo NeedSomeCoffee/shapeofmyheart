71d7bc1d3705424181eba0a5549747c6d1516ebe
NeedSomeCoffee <ivan.bai.sergeevich@gmail.com>
2018-08-10 19:06:15 +0300

    add logging output in main

--- src/main/java/edu/shapes/app/Application.java
+++ src/main/java/edu/shapes/app/Application.java
@@ -2,12 +2,24 @@ package edu.shapes.app;
 
 import java.util.logging.Logger;
 
+import edu.shapes.rectangular.Square;
 import edu.shapes.round.Circle;
+import edu.shapes.triangle.Triangle;
 
 public class Application {
-	private static final Logger LOGGER = Logger.getLogger("Main");
+	private static final Logger LOGGER = Logger.getLogger("Fain");
 	
 	public static void main(String[] args) {
 		Circle circle = new Circle(13.12);
+		
+		Square square = new Square(13.1);
+		
+		Triangle triangle = new Triangle(9.13, 8.34, 7.35);
+		
+		LOGGER.info(String.valueOf(circle.getArea()));
+		
+		LOGGER.info(String.valueOf(square.getArea()));
+		
+		LOGGER.info(String.valueOf(triangle.getArea()));
 	}
 }
